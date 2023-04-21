d27f48947b83266c380e9bd56cd9a32640543a58
motokoSAC <57342549+motokoSAC@users.noreply.github.com>
2023-04-21 14:15:11 +0800

    Update and rename LCD12864 to LCD12864.h

--- LCD12864.h
+++ LCD12864.h
@@ -2,9 +2,11 @@
 #define	__C12864_H_
 #define uchar unsigned char
 #define uint  unsigned int
+/*TS12864A-3 端口定义*/
 void Delay(unsigned int xms);
 void lcd12864_init();
 void lcd_wcmd(uchar cmd);
 void lcd_wdat(uchar dat);
 bit lcd_busy();
 #endif
+
